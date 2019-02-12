bd5eb2d45482f03a4502027060fad35b2ab093be
Victor Mutai <victorjambo@live.com>
2019-02-12 21:20:44 +0300

    Update Config.ts

--- src/Config.ts
+++ src/Config.ts
@@ -1,8 +1,3 @@
 export const config = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  databaseURL: process.env.DATABASEURL,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET
+  // TODO: Firebase configuration from web config
 };
